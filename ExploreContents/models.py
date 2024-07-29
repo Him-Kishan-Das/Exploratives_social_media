45a0c7a89d8f6e5d30d664e25ca0d84c6206a7d0
Him Kishan Das <himkishandas456@gmail.com>
2024-07-29 10:18:01 +0530

    development phase

--- ExploreContents/models.py
+++ ExploreContents/models.py
@@ -59,3 +59,14 @@ class Comments(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_comments')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comment')
     user_comment = models.TextField()
+
+class Follow(models.Model):
+    follower = models.ForeignKey(CustomUser, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(CustomUser, related_name='followers', on_delete=models.CASCADE)
+    create_at = models.DateField(auto_now=True)
+
+    class Meta:
+        unique_together = ('follower', 'following')
+    
+    def __str__(self):
+        return f"{self.follower.username} follows{self.following.username}"
